869c3e810fff52a004bbfd0e10610a04fded03f5
Margot Liard <margliard@gmail.com>
2021-11-09 16:25:04 +0100

    rails2 wikinimous setup

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+  # [:list, :show, :create, :update, :destroy]
+  def list
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Generating 10 random articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Books::Dune
+  )
+  article.save!
+end
+puts 'Done!'
